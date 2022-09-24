e377f4a4f5828559430519fdd45a96a0872ae6cb
Ipuch <puchaud.pierre@gmail.com>
2022-09-24 00:06:52 -0400

    forward dynamics only works went Q.v is vertical

--- examples/forward_dynamics.py
+++ examples/forward_dynamics.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from bioNC import NaturalSegment, NaturalCoordinates, SegmentNaturalCoordinates, NaturalCoordinates, SegmentNaturalVelocities
-from ode_solvers import RK4
+from ode_solvers import RK4, RK8, IRK
 
 # Let's create a segment
 my_segment = NaturalSegment(
@@ -11,7 +11,7 @@ my_segment = NaturalSegment(
     gamma=np.pi / 2,
     length=1,
     mass=1,
-    center_of_mass=np.array([0, 0, 0]),
+    center_of_mass=np.array([0, 0.01, 0]),
     inertia=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
 )
 
@@ -23,32 +23,40 @@ print(my_segment.rigidBodyConstraintJacobian(Qi))
 print(my_segment.rigidBodyConstraintJacobianDerivative(Qi.vector))
 
 # Let's create a motion now
+# u as x axis, w as z axis - this doesn't work
+# Qi = SegmentNaturalCoordinates.from_components(
+#     u=np.array([1, 0, 0]), rp=np.array([0, 0, 0]), rd=np.array([0, 1, 0]), w=np.array([0, 0, 1])
+# )
+# u as y axis - this works fine
 Qi = SegmentNaturalCoordinates.from_components(
-    u=np.array([1, 0, 0]), rp=np.array([0, 0, 0]), rd=np.array([0, 1, 0]), w=np.array([0, 0, 1])
+    u=np.array([0, 1, 0]), rp=np.array([0, 0, 0]), rd=np.array([0, 0, 1]), w=np.array([1, 0, 0])
 )
+# u as z axis - this doesn't work
+# Qi = SegmentNaturalCoordinates.from_components(
+#     u=np.array([0, 0, 1]), rp=np.array([0, 0, 0]), rd=np.array([1, 0, 0]), w=np.array([0, 1, 0]))
+
 Qidot = SegmentNaturalVelocities.from_components(
     udot=np.array([0, 0, 0]), rpdot=np.array([0, 0, 0]), rddot=np.array([0, 0, 0]), wdot=np.array([0, 0, 0])
 )
 
 my_segment.differential_algebraic_equation(Qi, Qidot)
 
-t_final = 0.2  # [s]
-time_steps = np.linspace(0, t_final, 21)
+t_final = 1  # [s]
+time_steps = np.linspace(0, t_final, 51)
 states_0 = np.concatenate((Qi.vector, Qidot.vector), axis=0)
 all_states = np.zeros((24, len(time_steps)))
-
+all_lambdas = np.zeros((6, len(time_steps)))
 
 def dynamics(t, states):
-    qddot = my_segment.differential_algebraic_equation(states[0:12], states[12:24])[0]
-    return np.concatenate((states[12:24], qddot), axis=0)
+    qddot, lambdas = my_segment.differential_algebraic_equation(states[0:12], states[12:24])
+    return np.concatenate((states[12:24], qddot), axis=0), lambdas
 
-
-all_states = RK4(time_steps, dynamics, states_0)
-
+all_states = RK4(time_steps, lambda t, states: dynamics(t, states)[0], states_0)
 
 defects = np.zeros((6, len(time_steps)))
 for i in range(len(time_steps)):
     defects[:, i] = my_segment.rigidBodyConstraint(all_states[0:12, i])
+    all_lambdas[:, i] = dynamics(time_steps[i], all_states[:, i])[1]
 
 import plotly.graph_objects as go
 
@@ -62,6 +70,25 @@ fig.add_trace(go.Scatter(x=time_steps, y=defects[4, :], name="defects[4]", mode=
 fig.add_trace(go.Scatter(x=time_steps, y=defects[5, :], name="defects[5]", mode="lines+markers"))
 fig.show()
 
+# display forces in time
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=time_steps, y=all_lambdas[0, :], name="lambda[0]", mode="lines+markers"))
+fig.add_trace(go.Scatter(x=time_steps, y=all_lambdas[1, :], name="lambda[1]", mode="lines+markers"))
+fig.add_trace(go.Scatter(x=time_steps, y=all_lambdas[2, :], name="lambda[2]", mode="lines+markers"))
+fig.add_trace(go.Scatter(x=time_steps, y=all_lambdas[3, :], name="lambda[3]", mode="lines+markers"))
+fig.add_trace(go.Scatter(x=time_steps, y=all_lambdas[4, :], name="lambda[4]", mode="lines+markers"))
+fig.add_trace(go.Scatter(x=time_steps, y=all_lambdas[5, :], name="lambda[5]", mode="lines+markers"))
+fig.show()
+
+
+
+def frame_args(duration):
+    return {
+            "frame": {"duration": duration},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"},
+        }
 
 fig = go.Figure(
     data=[
@@ -81,17 +108,11 @@ fig = go.Figure(
         ),
     ],
     layout=go.Layout(
-        updatemenus=[dict(type="buttons", buttons=[dict(label="Play", method="animate", args=[None])])],
-        transition={"duration": 0},
-        # sliders=[dict(active=1, transition={"duration": 0.01}, currentvalue={"prefix": "Time: "})],
-        # x, y and z axis limits are set to the same value for the sake of the animation
         scene=dict(
             xaxis=dict(range=[-2, 2], autorange=False),
             yaxis=dict(range=[-2, 2], autorange=False),
             zaxis=dict(range=[-2, 2], autorange=False),
         ),
-        # put a slider to control the time of each frame
-        sliders=[dict(steps=[dict(method="animate", args=[[f"frame_{i}"], dict(mode="immediate")])])],
         ),
     frames=[
         go.Frame(
@@ -115,4 +136,48 @@ fig = go.Figure(
         for i in range(len(time_steps))
     ],
 )
+
+
+sliders = [
+            {
+                "pad": {"b": 1, "t": t_final},
+                "len": 0.9,
+                "x": 1,
+                "y": 0,
+                "steps": [
+                    {
+                        "args": [[f.name], frame_args(0)],
+                        "label": str(k),
+                        "method": "animate",
+                    }
+                    for k, f in enumerate(fig.frames)
+                ],
+            }
+        ]
+
+# Layout
+fig.update_layout(
+    sliders=sliders,
+    title="Animation of the segment",
+    updatemenus=[{"buttons": [
+                    {
+                        "args": [None, frame_args(time_steps[1] - time_steps[0])],
+                        "label": "&#9654;", # play symbol
+                        "method": "animate",
+                    },
+                    {
+                        "args": [[None], frame_args(0)],
+                        "label": "&#9724;", # pause symbol
+                        "method": "animate",
+                    },
+                ],
+                "direction": "left",
+                "pad": {"r": 1, "t": t_final},
+                "type": "buttons",
+                "x": 0.1,
+                "y": 0,
+            }]
+)
+
 fig.show()
+

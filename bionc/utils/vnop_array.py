125683295339fd0879a10c1c8134f090a017b61e
Ipuch <puchaud.pierre@gmail.com>
2022-10-16 17:52:38 -0400

    vnop compatible with 3d

--- bionc/utils/vnop_array.py
+++ bionc/utils/vnop_array.py
@@ -26,6 +26,9 @@ def vnop_array(V: np.ndarray, e1: np.ndarray, e2: np.ndarray, e3: np.ndarray) ->
 
     if V.shape[0] != 3:
         raise ValueError("The vector must be expressed in 3D.")
+    if len(V.shape) == 1:
+        V = V[:, np.newaxis]
+
     if e1.shape[0] != 3:
         raise ValueError("The first vector of the non-orthogonal coordinate system must be expressed in 3D.")
     if e2.shape[0] != 3:
@@ -35,8 +38,8 @@ def vnop_array(V: np.ndarray, e1: np.ndarray, e2: np.ndarray, e3: np.ndarray) ->
 
     vnop = np.zeros(V.shape)
 
-    vnop[0] = np.sum(np.cross(e2, e3, axis=0) * V, 0) / np.sum(np.cross(e1, e2, axis=0) * e3, 0)
-    vnop[1] = np.sum(np.cross(e3, e1, axis=0) * V, 0) / np.sum(np.cross(e1, e2, axis=0) * e3, 0)
-    vnop[2] = np.sum(np.cross(e1, e2, axis=0) * V, 0) / np.sum(np.cross(e1, e2, axis=0) * e3, 0)
+    vnop[0, :] = np.sum(np.cross(e2, e3, axis=0) * V, 0) / np.sum(np.cross(e1, e2, axis=0) * e3, 0)
+    vnop[1, :] = np.sum(np.cross(e3, e1, axis=0) * V, 0) / np.sum(np.cross(e1, e2, axis=0) * e3, 0)
+    vnop[2, :] = np.sum(np.cross(e1, e2, axis=0) * V, 0) / np.sum(np.cross(e1, e2, axis=0) * e3, 0)
 
     return vnop

7d9983561aaa2bf6c931ccd8a41be576ae6e3049
Ipuch <pierre.puchaud@umontreal.ca>
2023-02-04 18:13:10 -0500

    the tests ik

--- examples/inverse_kinematics/inverse_kinematics.py
+++ examples/inverse_kinematics/inverse_kinematics.py
@@ -18,7 +18,8 @@ def main():
     # getting noisy markers for 200 frames
     markers = Markers.from_c3d(filename).to_numpy()[:3, :, :]  # 2 frames
     markers = np.repeat(markers, 100, axis=2)  # 2 x 100 frames
-    # markers = markers + np.random.normal(0, 0.01, markers.shape)  # add noise
+    np.random.seed(42)
+    markers = markers + np.random.normal(0, 0.01, markers.shape)  # add noise
 
     # you can import the class from bionc
     ik_solver = InverseKinematics(model, markers, solve_frame_per_frame=True)
@@ -26,18 +27,19 @@ def main():
     ik_solver = model.inverse_kinematics(markers, solve_frame_per_frame=True)
 
     tic0 = time.time()
-    Qopt = ik_solver.solve(method="sqpmethod")  # tend to be faster (with limited-memory hessian approximation)
+    Qopt_sqp = ik_solver.solve(method="sqpmethod")  # tend to be faster (with limited-memory hessian approximation)
     toc0 = time.time()
 
     tic1 = time.time()
-    Qopt = ik_solver.solve(method="ipopt")  # tend to find lower cost functions but may flip axis.
+    Qopt_ipopt = ik_solver.solve(method="ipopt")  # tend to find lower cost functions but may flip axis.
     toc1 = time.time()
 
     print(f"Time to solve 200 frames with sqpmethod: {toc0 - tic0}")
     print(f"time to solve 200 frames with ipopt: {toc1 - tic1}")
 
-    ik_solver.animate()
+    return ik_solver, Qopt_sqp, Qopt_ipopt
 
 
 if __name__ == "__main__":
-    main()
+    ik_solver, _, _ = main()
+    ik_solver.animate()

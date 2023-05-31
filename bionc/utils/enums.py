af70b2138a5cc59a0b7dbfcefc2bb728c6e94653
Ipuch <pierre.puchaud@umontreal.ca>
2023-05-31 00:05:35 -0400

    ISB and eulersequences

--- bionc/utils/enums.py
+++ bionc/utils/enums.py
@@ -11,3 +11,47 @@ class CartesianAxis(Enum):
     X = "X"
     Y = "Y"
     Z = "Z"
+
+
+class EulerSequence(Enum):
+    XYX = "xyx"
+    XZX = "xzx"
+    XYZ = "xyz"
+    XZY = "xzy"
+    YXY = "yxy"
+    YZX = "yzx"
+    YXZ = "yxz"
+    YZY = "yzy"
+    ZXZ = "zxz"
+    ZXY = "zxy"
+    ZYZ = "zyz"
+
+
+class ISBEulerSequence:
+    """ returns the corresponding enums
+        e.g. GLENO_HUMERAL returns EulerSequence.XYX
+    """
+    def __init__(self):
+        pass
+
+    @property
+    def GLENO_HUMERAL(self) -> EulerSequence:
+        return EulerSequence.YXY
+
+    @property
+    def SCAPULO_THORACIC(self) -> EulerSequence:
+        return EulerSequence.YXZ
+
+    @property
+    def ACROMIO_CLAVICULAR(self) -> EulerSequence:
+        return EulerSequence.YXZ
+
+    @property
+    def STERNO_CLAVICULAR(self) -> EulerSequence:
+        return EulerSequence.YXZ
+
+    @property
+    def THORACO_HUMERAL(self) -> EulerSequence:
+        return EulerSequence.YXY
+
+    # todo: add the rest of the joints

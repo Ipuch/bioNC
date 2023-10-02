fee87e8560f879e4195d2625e04bc4a26afba694
Anaaim <alexandre.naaim@univ-lyon1.fr>
2023-10-02 09:30:44 +0200

    Blacked

--- bionc/bionc_numpy/inverse_kinematics.py
+++ bionc/bionc_numpy/inverse_kinematics.py
@@ -439,55 +439,58 @@ class InverseKinematics:
 
         # Global will correspond to the squared sum of all the specifi residuals
         residuals_markers["Global"] = np.zeros((1, self.nb_frames))
-        residuals_makers_xyz["Global"] = np.zeros((3*self.nb_markers, self.nb_frames))
+        residuals_makers_xyz["Global"] = np.zeros((3 * self.nb_markers, self.nb_frames))
         residual_marker_max_name = [None] * self.nb_frames
         residuals_joints["Global"] = np.zeros((1, self.nb_frames))
         residuals_rigidity["Global"] = np.zeros((1, self.nb_frames))
 
         # The residual will also be calculated for each marker, joint and segment individually
-        for ind,key in enumerate(self.model.marker_names):
+        for ind, key in enumerate(self.model.marker_names):
             residuals_markers[key] = np.zeros((1, self.nb_frames))
             residuals_makers_xyz[key] = np.zeros((3, self.nb_frames))
 
-        for ind,key in enumerate(self.model.joint_names):
+        for ind, key in enumerate(self.model.joint_names):
             nb_constraint = self.model.joints[key].nb_constraints
             residuals_joints[key] = np.zeros((nb_constraint, self.nb_frames))
 
-        for ind,key in enumerate(self.model.segment_names):
+        for ind, key in enumerate(self.model.segment_names):
             residuals_rigidity[key] = np.zeros((6, self.nb_frames))
-
 
         for i in range(self.nb_frames):
             # Rigidity constraint
             phir_post_optim = self.model.rigid_body_constraints(NaturalCoordinates(self.Qopt[:, i]))
-            residuals_rigidity["Global"][:, i] = np.sqrt(np.dot(phir_post_optim,phir_post_optim))
-            for ind,key in enumerate(self.model.segment_names):
-                residuals_rigidity[key][:, i] = phir_post_optim[ind*6:(ind+1)*6]
+            residuals_rigidity["Global"][:, i] = np.sqrt(np.dot(phir_post_optim, phir_post_optim))
+            for ind, key in enumerate(self.model.segment_names):
+                residuals_rigidity[key][:, i] = phir_post_optim[ind * 6 : (ind + 1) * 6]
 
             # Kinematics constraints (associated with the joint of the model)
-            phik_post_optim= self.model.joint_constraints(NaturalCoordinates(self.Qopt[:, i]))
-            residuals_joints['Global'][:, i] = np.sqrt(np.dot(phik_post_optim,phik_post_optim))
+            phik_post_optim = self.model.joint_constraints(NaturalCoordinates(self.Qopt[:, i]))
+            residuals_joints["Global"][:, i] = np.sqrt(np.dot(phik_post_optim, phik_post_optim))
             nb_temp_constraint = 0
-            for ind,key in enumerate(self.model.joint_names):
+            for ind, key in enumerate(self.model.joint_names):
                 nb_constraint = self.model.joints[key].nb_constraints
-                if nb_constraint>0:
-                    residuals_joints[key][:, i] = phik_post_optim[nb_temp_constraint:nb_temp_constraint+nb_constraint]
+                if nb_constraint > 0:
+                    residuals_joints[key][:, i] = phik_post_optim[
+                        nb_temp_constraint : nb_temp_constraint + nb_constraint
+                    ]
                 nb_temp_constraint += nb_constraint
 
             # Marker constraints
             phim_post_optim = self.model.markers_constraints(
                 self.experimental_markers[:, :, i], NaturalCoordinates(self.Qopt[:, i]), only_technical=True
             )
-            residuals_markers["Global"][:, i] = np.sqrt(np.dot(phim_post_optim,phim_post_optim))
+            residuals_markers["Global"][:, i] = np.sqrt(np.dot(phim_post_optim, phim_post_optim))
             residuals_makers_xyz["Global"][:, i] = phim_post_optim
 
             for ind, key in enumerate(self.model.marker_names):
-                if ind==0:
-                    max=0
+                if ind == 0:
+                    max = 0
                     residual_marker_max_name[i] = key
 
-                residuals_markers[key][:, i] = np.sqrt(np.dot(phim_post_optim[ind*3:(ind+1)*3],phim_post_optim[ind*3:(ind+1)*3]))
-                residuals_makers_xyz[key][:, i] = phim_post_optim[ind*3:(ind+1)*3]
+                residuals_markers[key][:, i] = np.sqrt(
+                    np.dot(phim_post_optim[ind * 3 : (ind + 1) * 3], phim_post_optim[ind * 3 : (ind + 1) * 3])
+                )
+                residuals_makers_xyz[key][:, i] = phim_post_optim[ind * 3 : (ind + 1) * 3]
                 # Should we do this or do a huge matrix with all the residuals and then extract the max with a simple
                 # np.args(np.max(big_matrix) ?
                 if residuals_markers[key][:, i] > max:
@@ -496,9 +499,15 @@ class InverseKinematics:
 
         residuals_markers["max_name"] = residual_marker_max_name
 
-        residuals_markers["Full_Global"] = np.sqrt(np.dot(residuals_markers["Global"],np.transpose(residuals_markers["Global"])))
-        residuals_joints["Full_Global"] = np.sqrt(np.dot(residuals_joints["Global"],np.transpose(residuals_joints["Global"])))
-        residuals_rigidity["Full_Global"] = np.sqrt(np.dot(residuals_rigidity["Global"],np.transpose(residuals_rigidity["Global"])))
+        residuals_markers["Full_Global"] = np.sqrt(
+            np.dot(residuals_markers["Global"], np.transpose(residuals_markers["Global"]))
+        )
+        residuals_joints["Full_Global"] = np.sqrt(
+            np.dot(residuals_joints["Global"], np.transpose(residuals_joints["Global"]))
+        )
+        residuals_rigidity["Full_Global"] = np.sqrt(
+            np.dot(residuals_rigidity["Global"], np.transpose(residuals_rigidity["Global"]))
+        )
 
         residual_marker_final = dict()
         residual_marker_final["markers"] = residuals_markers
@@ -513,8 +522,8 @@ class InverseKinematics:
             markers=residual_marker_final,
             joints=residuals_joints,
             rigidity=residuals_rigidity,
-            #message=[sol.message for sol in self.list_sol],
-            #status=[sol.status for sol in self.list_sol],
-            #success=[sol.success for sol in self.list_sol],
+            # message=[sol.message for sol in self.list_sol],
+            # status=[sol.status for sol in self.list_sol],
+            # success=[sol.success for sol in self.list_sol],
         )
         return self.output

c65a455e11cbbd6f0b3b20df7fcfb357d98c3190
Anaaim <alexandre.naaim@univ-lyon1.fr>
2023-11-09 10:04:44 +0100

    blacked

--- bionc/utils/export_c3d_from_bionc_model.py
+++ bionc/utils/export_c3d_from_bionc_model.py
@@ -2,8 +2,9 @@ import ezc3d
 import numpy as np
 from bionc.bionc_numpy import NaturalCoordinates
 
+
 def get_points_ezc3d(acq):
-    '''
+    """
     Extract the points from a c3d file in a more readable format for user allowing to find the index of the points in the numpy array using text using a dictionnary.
     It allows to keep the data in the numpy array format for processing, but allow a more human way of using the data.(Using str instead of indexes)
     Parameters
@@ -18,19 +19,20 @@ def get_points_ezc3d(acq):
         The name of the points
     points_ind : dict
         The dictionnary allowing to find the index of the points in the numpy array
-    '''
+    """
 
-    points_name = acq['parameters']['POINT']['LABELS']['value']
+    points_name = acq["parameters"]["POINT"]["LABELS"]["value"]
 
-    points_data = acq['data']['points'][0:3, :, :]
+    points_data = acq["data"]["points"][0:3, :, :]
     points_ind = dict()
     for index_point, name_point in enumerate(points_name):
         points_ind[name_point] = index_point
 
     return points_data, points_name, points_ind
 
+
 def add_point_from_dictionary(acq, point_to_add):
-    '''
+    """
 
     Parameters
     ----------
@@ -42,13 +44,13 @@ def add_point_from_dictionary(acq, point_to_add):
     -------
     acq : ezc3d.c3d
         The c3d file with the points added
-    '''
+    """
 
     points, points_name, points_ind = get_points_ezc3d(acq)
     # copy points informations
     new_list = points_name.copy()
-    new_array = acq['data']['points']
-    nb_frame = acq['data']['points'].shape[2]
+    new_array = acq["data"]["points"]
+    nb_frame = acq["data"]["points"].shape[2]
 
     for ind_point, (name_point, value_point) in enumerate(point_to_add.items()):
         new_point = np.zeros((4, 1, nb_frame))
@@ -58,29 +60,26 @@ def add_point_from_dictionary(acq, point_to_add):
         new_array = np.append(new_array, new_point, axis=1)
 
     # Add the new points to the c3d file
-    acq['parameters']['POINT']['LABELS']['value'] = new_list
-    acq['parameters']['POINT']['DESCRIPTIONS']['value'] = new_list.copy()
+    acq["parameters"]["POINT"]["LABELS"]["value"] = new_list
+    acq["parameters"]["POINT"]["DESCRIPTIONS"]["value"] = new_list.copy()
 
     # Some parameters need to be modified for the c3d to be working
     temp_residuals = np.zeros((1, new_array.shape[1], new_array.shape[2]))
-    temp_residuals[0, :acq['data']['meta_points']['residuals'].shape[1],
-                   :] = acq['data']['meta_points']['residuals']
-    old_camera_mask = acq['data']['meta_points']['camera_masks']
-    temp_camera_mask = np.zeros(
-        (old_camera_mask.shape[0], new_array.shape[1], old_camera_mask.shape[2]))
+    temp_residuals[0, : acq["data"]["meta_points"]["residuals"].shape[1], :] = acq["data"]["meta_points"]["residuals"]
+    old_camera_mask = acq["data"]["meta_points"]["camera_masks"]
+    temp_camera_mask = np.zeros((old_camera_mask.shape[0], new_array.shape[1], old_camera_mask.shape[2]))
     temp_camera_mask[:, :, :] = False
-    temp_camera_mask[:, :acq['data']['meta_points']
-                     ['residuals'].shape[1], :] = old_camera_mask
-    acq['data']['meta_points']['residuals'] = temp_residuals
-    acq['data']['meta_points']['camera_masks'] = temp_camera_mask.astype(
-        dtype=bool)
+    temp_camera_mask[:, : acq["data"]["meta_points"]["residuals"].shape[1], :] = old_camera_mask
+    acq["data"]["meta_points"]["residuals"] = temp_residuals
+    acq["data"]["meta_points"]["camera_masks"] = temp_camera_mask.astype(dtype=bool)
     # Add the new analogs to the c3d file used for the type 2 platform
-    acq['data']['points'] = new_array
+    acq["data"]["points"] = new_array
 
     return acq
 
-def export_c3d_biomechanical(model,  Q,filename_export, filename_initial=None, fq_file=None):
-    '''
+
+def export_c3d_biomechanical(model, Q, filename_export, filename_initial=None, fq_file=None):
+    """
 
     Parameters
     ----------
@@ -98,7 +97,7 @@ def export_c3d_biomechanical(model,  Q,filename_export, filename_initial=None, f
     Returns
     -------
 
-    '''
+    """
 
     if Q is not isinstance(Q, NaturalCoordinates):
         Q = NaturalCoordinates(Q)
@@ -107,8 +106,8 @@ def export_c3d_biomechanical(model,  Q,filename_export, filename_initial=None, f
 
     dict_to_add = dict()
     # We add the technical markers
-    for ind_marker,name_marker in enumerate(model.marker_names):
-        dict_to_add[f"{name_marker}_optim"] = model_markers[:,ind_marker,:]
+    for ind_marker, name_marker in enumerate(model.marker_names):
+        dict_to_add[f"{name_marker}_optim"] = model_markers[:, ind_marker, :]
 
     # We add the segment rp,rd,u,w to the c3d file
     for s in range(Q.nb_qi()):
@@ -124,14 +123,13 @@ def export_c3d_biomechanical(model,  Q,filename_export, filename_initial=None, f
         dict_to_add[f"w_{name_segment}"] = rd_temp + w_temp / 10
 
     if filename_initial is None:
-        original_c3d = ezc3d.ezc3dRead();
+        original_c3d = ezc3d.ezc3dRead()
         if fq_file is not None:
-            original_c3d.parameters.POINT.RATE.DATA = fq_file;
+            original_c3d.parameters.POINT.RATE.DATA = fq_file
         else:
-            original_c3d.parameters.POINT.RATE.DATA = 100;
+            original_c3d.parameters.POINT.RATE.DATA = 100
     else:
         original_c3d = ezc3d.c3d(filename_initial)
 
     add_point_from_dictionary(original_c3d, dict_to_add)
     original_c3d.write(filename_export)
-

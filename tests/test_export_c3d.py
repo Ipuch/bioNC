da42b2ad5f22b6450998ef1d517e035dbd640954
Ipuch <pierre.puchaud@umontreal.ca>
2024-03-01 17:31:59 -0500

    tests(drafting it for @ANaaim)

--- tests/test_export_c3d.py
+++ tests/test_export_c3d.py
@@ -2,49 +2,33 @@ import os
 
 import ezc3d
 import numpy as np
-import pytest
 from pyomeca import Markers
 
+from bionc.bionc_numpy.inverse_kinematics import InverseKinematics
 from bionc.utils.c3d_export_utils import (
-    add_natural_coordinate_to_c3d,
-    add_technical_markers_to_c3d,
     get_points_ezc3d,
 )
 from .utils import TestUtils
 
 
-@pytest.mark.parametrize(
-    "bionc_type",
-    [
-        "numpy",
-        "casadi",
-    ],
-)
-def test_export_c3d(bionc_type):
+def test_export_c3d():
     bionc = TestUtils.bionc_folder()
     module = TestUtils.load_module(bionc + "/examples/model_creation/right_side_lower_limb.py")
-
     # Generate c3d file
     filename = module.generate_c3d_file()
-    # Generate model
-    model = module.model_creation_from_measured_data(filename)
-
-    markers = Markers.from_c3d(filename).to_numpy()
 
     acq = ezc3d.c3d(filename)
-    add_technical_markers_to_c3d(acq, model, model.Q_from_markers(markers[:, :, :]))
-    add_natural_coordinate_to_c3d(acq, model, model.Q_from_markers(markers[:, :, :]))
     # delete c3d file
     os.remove(filename)
     points_data, points_name, points_ind = get_points_ezc3d(acq)
     # We test some value of the c3d file for both the marker and the natural coordinate
     TestUtils.assert_equal(
-        points_data[:, points_ind["RKNE_optim"], :],
+        points_data[:, points_ind["RKNE"], :],
         np.array([[0.28976313, 0.29850912], [1.40092926, 1.40187666], [0.47585389, 0.47547166]]),
     )
 
     TestUtils.assert_equal(
-        points_data[:, points_ind["RKNI_optim"], :],
+        points_data[:, points_ind["RKNI"], :],
         np.array([[0.2888405, 0.29766819], [1.29665239, 1.29736223], [0.4304646, 0.43063049]]),
     )
 
@@ -101,3 +85,15 @@ def test_export_c3d(bionc_type):
         "w_FOOT",
     ]
     assert set(list_name_marker).issubset(points_name)
+
+
+def test_ik_export():
+    bionc = TestUtils.bionc_folder()
+    module = TestUtils.load_module(bionc + "/examples/model_creation/right_side_lower_limb.py")
+    filename = module.generate_c3d_file()
+    model = module.model_creation_from_measured_data(filename)
+    markers = Markers.from_c3d(filename).to_numpy()[:3, :, :]
+    MOCK_IK = InverseKinematics(model=model, experimental_markers=markers)
+
+    MOCK_IK.Qopt = np.random.random((model.nb_Q, 2))
+    MOCK_IK.export_in_c3d(filename)

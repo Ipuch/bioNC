561725f7a3c8ba6cb7cab6d745d889e82d762f6e
Ipuch <pierre.puchaud@umontreal.ca>
2024-03-15 11:29:31 -0400

    black

--- bionc/bionc_numpy/inverse_kinematics.py
+++ bionc/bionc_numpy/inverse_kinematics.py
@@ -286,8 +286,7 @@ class InverseKinematics:
                 raise ValueError("Either, set frame_per_frame == True or use InitialGuessModeType.USER_PROVIDED.")
             if Q_init.shape[1] != 1:
                 raise ValueError(
-                    f"Please provide only the first frame of Q_init."
-                    f"Currently, Q_init.shape[1] = {Q_init.shape[1]}."
+                    f"Please provide only the first frame of Q_init." f"Currently, Q_init.shape[1] = {Q_init.shape[1]}."
                 )
 
             return Q_init
